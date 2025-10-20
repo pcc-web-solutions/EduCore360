1721be503f50497679b985835b92a8eb5baefe2a
pcc-web-solutions <pccws.limited@gmail.com>
2025-10-20 12:03:46 +0300

    remove test.php

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-require "server.php";
-$timetable = new Timetable();
-if($timetable->generate($dmo->getSchInfo($_SESSION['user'])['data'],'1')){
-    echo "Timetable successfully generated"."<br>"."Go to options to export it.";
-}else{
-    die('Unable to generate timetable'."<br>"."View <a href='#' target='_blank'> event log </a> to see what happened.");
-}
